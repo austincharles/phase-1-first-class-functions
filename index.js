22d37a783ee5b188eae25c507fb2efd1d58c2c4a
Austin Holeman <austinholeman@gmail.com>
2022-03-01 14:19:17 -0800

    commit tree 2b8ea302375f1126524698a380a40e39092ac549

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback) {
+    return callback();
+}
+
+function returnsANamedFunction() {
+    const namedFn = () => console.log("this is a named function")
+    return namedFn;
+ }
+
+ function returnsAnAnonymousFunction() {
+    return () => console.log("this is an anonymous function")
+ }
